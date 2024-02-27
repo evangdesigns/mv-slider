4ef5cc8e183c3feb0fc2296ade36b425e6084877
Evan G <evangdesigns@gmail.com>
2024-02-27 13:02:17 -0600

    initial wp plugin setup

---
+++ views/mv-slider_metabox.php
@@ -0,0 +1,44 @@
+<?php
+//gets all info related to current post
+  $meta = get_post_meta( $post->ID );
+
+// gets specific keys associated with current post
+  $link_text = get_post_meta( $post->ID, 'mv_slider_link_text', true );
+  $link_url = get_post_meta( $post->ID, 'mv_slider_link_url', true );
+
+// displays everything associated with inputted variable on the DOM
+  //var_dump( $meta )
+?>
+
+<table class="form-table mv-slider-metabox">
+  <input type="hidden" name="mv_slider_nonce" value="<?php echo wp_create_nonce( 'mv_lider_nonce' ) ?>">
+  <tr>
+    <th>
+      <label for="mv_slider_link_text">Link Text</label>
+    </th>
+    <td>
+      <input
+        type="text"
+        name="mv_slider_link_text"
+        id="mv_slider_link_text"
+        class="regular-text link-text"
+        value="<?php echo ( isset( $link_text) ) ? esc_html( $link_text ) : ''; ?>"
+        required
+      >
+    </td>
+  </tr>
+  <tr>
+    <th>
+      <label for="mv_slider_link_url">Link Text</label>
+    </th>
+    <td>
+      <input
+        type="url"
+        name="mv_slider_link_url"
+        id="mv_slider_link_url"
+        class="regular-text link-url"
+        value="<?php echo ( isset( $link_url ) ) ? esc_url( $link_url ) : ''; ?>"
+        required>
+    </td>
+  </tr>
+</table>
