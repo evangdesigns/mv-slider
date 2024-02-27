ee2aa0e4e59cc698f9f97a94ff8d6263fa0b779b
Evan G <evangdesigns@gmail.com>
2024-02-27 15:01:22 -0600

    Added submenu pages

--- mv-slider.php
+++ mv-slider.php
@@ -39,6 +39,8 @@ if ( !class_exists( 'MV_Slider' )){
     function __construct(){
       $this->define_constants();
 
+      add_action( 'admin_menu', array( $this, 'add_menu') );
+
       require_once( MV_SLIDER_PATH . 'post-types/class.mv-slider-cpt.php');
       $MV_Slider_Post_Type = new MV_Slider_Post_Type();
     }
@@ -59,6 +61,39 @@ if ( !class_exists( 'MV_Slider' )){
 
     public static function uninstall(){
 
+    }
+    public function add_menu(){
+      add_menu_page(
+        'MV Slider Options',
+        'MV Slider',
+        'manage_options',
+        'mv_slider_admin',
+        array( $this, 'mv_slider_settings_page'),
+        'dashicons-images-alt2',
+        10
+      );
+
+      add_submenu_page(
+        'mv_slider_admin',
+        'Manage Slides',
+        'Manage Slides',
+        'manage_options',
+        'edit.php?post_type=mv-slider',
+        null,
+        null
+      );
+      add_submenu_page(
+        'mv_slider_admin',
+        'Add New Slide',
+        'Add New Slide',
+        'manage_options',
+        'post-new.php?post_type=mv-slider',
+        null,
+        null
+      );
+    }
+    public function mv_slider_settings_page(){
+      echo 'This is the settings page';
     }
   }
 }
